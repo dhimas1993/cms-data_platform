4e4a5cc24812410159eb5d5b043bdc87822f0054
hartatoadi <dhimashertianto@gmail.com>
2021-05-11 11:49:14 +0700

    fix cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(cors())
+app.use(cors({origin: true, credentials: true}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
