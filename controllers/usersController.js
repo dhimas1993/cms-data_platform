879d78e34f99ff8e9ae1673258b7e7241533b974
Dhimas Hertianto <50478200+dhimas1993@users.noreply.github.com>
2021-07-15 18:27:39 +0700

    fix subscribe type link

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -318,6 +318,7 @@ module.exports = {
             
             let pro = []
             let free = []
+            let enterprise = []
 
             for (let i = 0; i < link.length; i++) {
                 const item = link[i].subscribe.name;
@@ -327,13 +328,17 @@ module.exports = {
                     free.push(data)
                 } else if(item == 'PRO'){
                     pro.push(data)
+                } else if(item == 'ENTERPRISE'){
+                    enterprise.push(data)
                 }
             }
 
             if(response.subscribe.name == 'FREE'){
                 res.status(200).json(free)
-            } else {
+            } else if(response.subscribe.name == 'PRO'){
                 res.status(200).json(pro)
+            } else {
+                res.status(200).json(enterprise)
             }
         } catch (error) {
             res.status(500).json(error.message)
